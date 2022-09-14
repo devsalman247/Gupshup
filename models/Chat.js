1c4126141184f8a5698160c52ee6c9a61b49aa79
devsalman247 <salman247.dev@gmail.com>
2022-09-14 18:09:13 +0500

    updated chat model

--- models/Chat.js
+++ models/Chat.js
@@ -2,7 +2,34 @@ const mongoose = require('mongoose');
 
 const ChatSchema = new mongoose.Schema(
     {
-        
+        participants : [{
+            user : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref  : 'Friend'
+            },
+            group : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref  : 'Group'
+            }
+        }],
+        messages : [{
+            body : {
+                type : String
+            },
+            sentBy : {
+                type : mongoose.Schema.Types.ObjectId,
+                ref  : 'User'
+            },
+            sentAt   : Date,
+            isStarred : {
+                type    : Boolean,
+                default : false
+            },
+            isLiked : {
+                type    : Boolean,
+                default : false
+            }
+        }]
     },
     {timestamps : true}
 );
