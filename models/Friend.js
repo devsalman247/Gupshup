edd8cfd2d457f8fc8ae8c1e840701bf408564021
devsalman247 <salman247.dev@gmail.com>
2022-09-26 20:25:02 +0500

    updated routes

--- models/Friend.js
+++ models/Friend.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const FriendSchema = new mongoose.Schema(
     {
-        name : {
+        friendId : {
             type : mongoose.Schema.Types.ObjectId,
             ref  : 'User',
             required : true
--- routes/api/friend.js
+++ routes/api/friend.js
@@ -15,22 +15,42 @@ router.post('/add', (req, res, next) => {
         }else if(!user) {
             res.send("User is not registered.");
         }else {
-            const friend = new Friend();
-            friend.name = user.id;
-            friend.save()
-            .then(friendData => {
-                User.findById(req.user.id, (error, data) => {
-                    if(error) {
+            User.findById(req.user.id, (error, myProfile) => {
+                if(error) {
+                    res.send({error : {message : error.message}});
+                }
+                const index = user.friends.findIndex(obj => obj.requestId===myProfile.id);
+                if(index>-1 && user.friends[index].status===1) {
+                    res.send({message : "You've already sent a friend request..Please wait for a reply!!"});
+                }else if(index>-1 && user.friends[index].status===2) {
+                    res.send({message : "You're already friends."});
+                }else if(index>-1 && user.friends[index].status===0) {
+                    const friend = new Friend();
+                    friend.friendId = user.id;
+                    friend.save()
+                    .then(friendData => {
+                        user.friends[index].friendDetails = friendData;
+                        user.save();
+                        res.send({message : "You're now friends!!"});
+                    })
+                    .catch(error => {
                         res.send({error : {message : error.message}});
-                    }
-                    data.friends.push(friendData);
-                    data.save();
-                })
-                res.send(friendData);
+                    })
+                }else {
+                    user.friends.push({
+                        status : 1,
+                        requestId : myProfile.id
+                    });
+                    user.save();
+                    myProfile.friends.push({
+                        status : 0,
+                        requestId : user.id
+                    });
+                    myProfile.save();
+                    res.send({message : "Request sent successfully.."});
+                }
             })
-            .catch(error => {
-                res.send({error : {message : error.message}});
-            })
+
         }
     })
 });
